1ce48b710de4317ad5a5164e5ae702da6e3995f4
Gisele Ribeiro <126531529+giselesribeiro@users.noreply.github.com>
2023-05-15 14:55:24 -0300

    aulas de JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro está a ${vel} km/h`) 
+if (vel > 60.5){
+    console.log (`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre com o sinto de segurança`)
