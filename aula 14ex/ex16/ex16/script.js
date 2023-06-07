f35fbef1eb8114eb73433c7cc7b81850ad76cbc2
Gisele Ribeiro <126531529+giselesribeiro@users.noreply.github.com>
2023-06-07 17:00:10 -0300

    novos aprendizados

---
+++ aula 14ex/ex16/ex16/script.js
@@ -0,0 +1,14 @@
+
+function contar() {
+var ini = document.getElementById('iinicio')
+var fim = document.getElementById('ifim')
+var passo = document.getElementById('ipasso')
+var res = document.getElementById('res')
+
+if (ini == 0){
+    alert('ERRO! FALTAM DADOS!')
+} else{
+    res.innerHTML = 'Contando:'
+}
+
+}
