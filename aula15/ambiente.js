bc26a141d1871a12062c7877826503b44440ac9a
Gisele Ribeiro <giselesribeiro8@gmail.com>
2023-07-04 20:07:22 -0300

    aula 15 concluída

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,8 @@
-var num = []
+var num = [5,8,2,9,3]
+num.push(1)
 
-console.log(`nosso var é ${num}`)
+console.log (num)
+//console.log(num.sort())
+console.log(`A primeira posição é o ${num[0]}`)
+let pos = num.indexOf(3)
+console.log(`o valor 3 é ${pos}`)
