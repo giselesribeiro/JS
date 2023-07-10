e200a7c24367445aeb306fa219cffdd89362259b
Gisele Ribeiro <giselesribeiro8@gmail.com>
2023-07-10 20:29:21 -0300

    aula 16 com exercício 07 em andamento

---
+++ aula16/ex007/script.js
@@ -0,0 +1,6 @@
+var num = document
+
+
+function adicionar(){
+
+}
---
+++ aula16/funçao02.js
@@ -0,0 +1,9 @@
+/* function somar(n1,n2){
+    return n1 + n2
+}
+console.log(somar(3,4)) */
+
+function somar(n1=0,n2=0){
+    return n1 + n2
+}
+console.log(somar(3))
