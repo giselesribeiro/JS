54c584586f1ae4351b896bb1bb69b2f8a0319022
Gisele Ribeiro <giselesribeiro8@gmail.com>
2023-07-11 18:38:43 -0300

    parte 1 do exercício 007 concluído

--- aula16/ex007/script.js
+++ aula16/ex007/script.js
@@ -1,6 +1,29 @@
-var num = document
+var num = document.getElementById('fnum')
+var lista = document.getElementById('flista')
+var res = document.getElementById('res')
+var valores = []
 
+function eNumero(n){
+   if(Number(n) >= 1 && Number(n) <= 100 ){
+     return true
+   }else{
+      return false
+   }
+}
+function naLista(n, l){
+   if(l.indexOf(Number(n)) != -1){
+     return true
+   }else{
+      return false
+   }
+}
 
-function adicionar(){
+function adicionar() {
+   if(eNumero(num.value) && !naLista(num.value, valores)){
+      window.alert('Tudo ok')
+      valores.push()
 
+   }else{
+      window.alert('Numero inválido')
+   }
 }
