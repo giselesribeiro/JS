2ece6f334e67d244ad31775a961d2eccd428d4a5
Gisele Ribeiro <giselesribeiro8@gmail.com>
2023-07-12 18:35:49 -0300

    conclusao do último desafio do curso básico de JavaScript

--- aula16/ex007/script.js
+++ aula16/ex007/script.js
@@ -25,8 +25,39 @@ function adicionar() {
       var item = document.createElement('option')
       item.text = (`Valor ${num.value} adicionado.`)
       lista.appendChild(item)
+      res.innerHTML = ''
 
    }else{
       window.alert('Numero inválido')
+   }
+   num.value = ''
+   num.focus()
+}
+function finalizar(){
+   if(valores.length == 0){
+      alert('Adicione valores antes de finalizar')
+   }else{
+      var tot = valores.length
+      var maior = valores[0]
+      var menor = valores [0]
+      var soma = 0
+      var media = 0
+      for(var pos in valores){
+         soma += valores[pos]
+         if(valores[pos] > maior)
+           maior = valores[pos]
+         if(valores[pos] < menor)
+           menor = valores[pos]
+      }
+      media = soma / tot
+      res.innerHTML = ''
+      res.innerHTML +=` <p> Ao todo, temos ${tot} números cadastrados</p>`
+      res.innerHTML += `<p> O maior valor informado foi ${maior}</p>`
+      res.innerHTML += `<p> O menor valor informado foi ${menor}</p>`
+      res.innerHTML += `<p> A soma desses valores resulta em ${soma}</p>`
+      res.innerHTML += `<p> A média desses valores é ${media}</p>`
+
+      
+
    }
 }
